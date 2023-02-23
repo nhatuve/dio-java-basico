b287166c6b099373795a1b98a4dddc1097336544
nhatuve <nhatuveoffice@gmail.com>
2023-02-23 15:55:06 +0200

    Documentação do código java

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia antentamenente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Carlos Nhatuve
+ * @version 1.0
+ * @since 23/02/2023
+ */
+public class Calculadora{
+    /**
+     * Este método é usado para somar dois números inteiros
+     * @param numeroA este é o primeiro parâmetro do método
+     * @param numeroB este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+    public int somar(int numeroA, int numeroB){
+        return numeroA + numeroB;
+        
+    }
+}
---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,18 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Comentário de única linha
+
+        /*
+         * Comentario de multiplas linhas,
+         * quando é necessario detalhar mais 
+         */
+    }
+
+    /**
+     * Este comentário de duas estrelinhas é para aspectos de 
+     * documentação do código.
+     */
+    public void metodo(){
+
+    }
+}
