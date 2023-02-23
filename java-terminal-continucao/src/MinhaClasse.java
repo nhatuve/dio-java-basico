8bc3dd6c1693cd48156c3103f4b348931f8514a7
nhatuve <nhatuveoffice@gmail.com>
2023-02-24 01:06:59 +0200

    Java Basico

---
+++ java-terminal-continucao/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Ola pessoal, Executar programas, sem precisar usar uma IDE.");
+    }
+}
---
+++ java-terminal-continucao/src/SobreMim.java
@@ -0,0 +1,10 @@
+public class SobreMim {
+    public static void main(String[] args) throws Exception {
+        
+        String nome = args [0];
+        String apelido = args [1];
+        int idade = Integer.valueOf(args [2]);
+        double altura = Double.valueOf(args [3]);
+        
+    }
+}
